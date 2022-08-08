c7ce39b304cedf426e5feb096db3a4608513217e
nilssvensson <nils27school@gmail.com>
2022-08-08 14:58:55 -0400

    commit tree d5095e50c32121f365e1dcd0aa8e8506f73810e8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+  const cart = []
+  
+  for (let i = 0; i < names.length; i++) {
+   cart.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+
+  return cart;
+}
+
+function countDown(number) {
+
+while (number >= 0) {
+  console.log(number--);
+}
+return number;
+}
